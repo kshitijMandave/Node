76ea6544035068928442c2010035b6b02d393dcd
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-16 14:50:55 +0530

    MVC Implemented and Updated

--- 08_MVC/controllers/user.js
+++ 08_MVC/controllers/user.js
@@ -1,10 +1,56 @@
 const User = require("../models/user");
 
-async function handleGetAllUsers() {
+async function handleGetAllUsers(req, res) {
   const allUsers = await User.find({});
   return res.json(allUsers);
 }
 
+async function handlegetUserById(req, res) {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ error: "user not found" });
+  return res.json(user);
+}
+
+async function handleUpdaeUserById(req, res) {
+  await User.findByIdAndUpdate(req.params.id, { lastName: Changed });
+  return res.json({ status: "Success" });
+}
+
+async function handleDeleteUserById(req, res) {
+  await User.findByIdAndDelete(req.params.id);
+  return res.json({ status: "Success" });
+}
+
+async function handleCreateNewUser(params) {
+  const body = req.body;
+
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.jobTitle
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const result = await User.create({
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    password: body.password,
+    jobTitle: body.jobTitle,
+    gender: body.gender,
+  });
+
+  return res.status(201).json({ status: "success", id: result._id });
+}
+
 module.exports = {
   handleGetAllUsers,
+  handlegetUserById,
+  handleUpdaeUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
 };
--- 08_MVC/routes/userRoute.js
+++ 08_MVC/routes/userRoute.js
@@ -1,52 +1,23 @@
 const express = require("express");
 
-const { handleGetAllUsers } = require("../controllers/user");
+const {
+  handleGetAllUsers,
+  handlegetUserById,
+  handleUpdaeUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+} = require("../controllers/user");
 
 const router = express.Router();
 
 // Routes
-router.get("/", handleGetAllUsers);
+router.route("/").get(handleGetAllUsers).post(handleCreateNewUser);
 
 router
   .route("/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-  })
-  .patch((req, res) => {
-    return res.json({ status: "pending" });
-  })
-  .delete((req, res) => {
-    return res.json({ status: "pending" });
-  });
-
-router.post("/", async (req, res) => {
-  try {
-    const body = req.body;
-
-    if (!body || !body.first_name || !body.email || !body.password) {
-      return res.status(400).json({ error: "All fields are required" });
-    }
-
-    const result = await User.create({
-      first_name: body.first_name,
-      last_name: body.last_name,
-      email: body.email,
-      password: body.password,
-      jobTitle: body.jobTitle,
-      gender: body.gender,
-    });
-
-    console.log("Result:", result);
-    return res.status(201).json({ status: "success", data: result });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .json({ error: "Server error", details: err.message });
-  }
-});
+  .get(handlegetUserById)
+  .patch(handleUpdaeUserById)
+  .delete(handleDeleteUserById);
 
 // exporting the router
 module.exports = router;
